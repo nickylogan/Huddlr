123f3185d90c64c8dc64ba699ded232765974e54
NickyLogan <laurentiusdominick@yahoo.com>
2018-10-25 02:16:30 +0700

    Add form handling scripts

--- src/client/landing.js
+++ src/client/landing.js
@@ -1,2 +1,22 @@
 import url from '../../resources/sass/style.scss';
 import $ from 'jquery';
+
+(function () {
+    $('#roomWorld').click(() => {
+        $('#room-id').attr('disabled', 'disabled');
+    });
+    $('#roomPrivate').click(() => {
+        $('#room-id').removeAttr('disabled');
+    });
+
+    let form = $('#landingForm');
+    form.submit((e) => form.find('input[name="type"]').length > 0);
+    $('#joinRoom').click( function () {
+        form.append($(`<input type="hidden" name="type" value="join">`));
+        form.submit();
+    });
+    $('#createRoom').click( function () {
+        form.append($(`<input type="hidden" name="type" value="create">`));
+        form.submit();
+    })
+})()
