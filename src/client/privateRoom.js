98bb118bfe10e96c9f9366fb0d9e11ae00ae4df8
NickyLogan <laurentiusdominick@yahoo.com>
2018-10-29 10:37:24 +0700

    Add UI tests for user list

--- src/client/privateRoom.js
+++ src/client/privateRoom.js
@@ -5,3 +5,25 @@ function log(message) {
 }
 
 let chatUI = new ChatUI(log).initialize();
+
+// THESE ARE UI TESTS. REMOVE LATER!
+
+let ids = [];
+
+setInterval(function() {
+    let k = utils.randomInt(0,3);
+    if(k >= 1) {
+        let id = utils.makeid(6);
+        ids.push(id);
+        chatUI.appendUser({
+            id: id,
+            name: id,
+            color: utils.randomColor(),
+        });
+    } else {
+        let idx = utils.randomInt(0, ids.length-1);
+        let id = ids[idx];
+        ids.splice(idx,1);
+        chatUI.removeUser(id);
+    }
+}, 2000);
--- src/client/worldRoom.js
+++ src/client/worldRoom.js
@@ -1,7 +1,31 @@
 import ChatUI from './chatUI';
+import $ from 'jquery';
+import * as utils from './utils';
 
 function log(message) {
     console.log("World: " + message);
 }
 
 let chatUI = new ChatUI(log).initialize();
+
+// THESE ARE UI TESTS. REMOVE LATER!
+
+let ids = [];
+
+setInterval(function() {
+    let k = utils.randomInt(0,3);
+    if(k >= 1) {
+        let id = utils.makeid(6);
+        ids.push(id);
+        chatUI.appendUser({
+            id: id,
+            name: id,
+            color: utils.randomColor(),
+        });
+    } else {
+        let idx = utils.randomInt(0, ids.length-1);
+        let id = ids[idx];
+        ids.splice(idx,1);
+        chatUI.removeUser(id);
+    }
+}, 2000);
