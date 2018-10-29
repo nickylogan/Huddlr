f0e3a12e59295f3875dfd31df35dfdd7c6ffb975
NickyLogan <laurentiusdominick@yahoo.com>
2018-10-29 10:15:07 +0700

    Add more utility functions

--- src/client/utils.js
+++ src/client/utils.js
@@ -24,3 +24,27 @@ export function getTerminalTime() {
     let s = ('0' + d.getSeconds()).slice(-2);
     return `[${y}-${mm}-${dd} ${h}:${m}:${s}]`;
 }
+
+export function makeid(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < length; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+export function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function randomColor() {
+    let r = randomInt(0, 128);
+    let g = randomInt(0, 128);
+    let b = randomInt(0, 128);
+    r = ('0' + r.toString(16)).slice(-2);
+    g = ('0' + g.toString(16)).slice(-2);
+    b = ('0' + b.toString(16)).slice(-2);
+    return `#${r + g + b}`;
+}
